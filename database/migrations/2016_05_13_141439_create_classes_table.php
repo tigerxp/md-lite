0f30625662a676ea7b55d83b65f0e248fc1481ee
tiger <s.garmash@softerra.com>
2016-05-13 19:28:45 +0300

    Added: basic Laravel, a couple of migrations

---
+++ database/migrations/2016_05_13_141439_create_classes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('parent_id')->unsigned()->index();
+            $table->text('sub_list');
+            $table->text('parent_list');
+            $table->integer('nolog');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('classes');
+    }
+}
---
+++ database/migrations/2016_05_13_142233_create_objects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateObjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('objects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('class_id')->unsigned()->index();
+            $table->text('description');
+            $table->integer('location_id')->unsigned();
+            $table->integer('keep_history');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('objects');
+    }
+}
